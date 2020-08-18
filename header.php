3ea03c8c05458ac84f9011be9e1d73fa590a549e
lisaelyacoubi <lisa.elyacoubi@gmail.com>
2020-08-18 16:41:46 +0200

    add basic structure html for first wordpress theme

---
+++ header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <?php wp_head() ?>
+  </head>
+  <body>
+    <div class="container">
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php get_header() ?>
+
+<h1 class="text-uppercase">Mon premier thème</h1>
+
+<?php get_footer() ?>
