35c81367c63c7c51a98b47a9afd32bd27573d78b
lisaelyacoubi <lisa.elyacoubi@gmail.com>
2020-08-18 16:42:38 +0200

    add functions to stylish theme with bootstrap

---
+++ functions.php
@@ -0,0 +1,28 @@
+<?php
+//add_theme_support('title-tag');
+
+function montheme_supports() {
+  add_theme_support('title-tag');
+}
+
+function montheme_register_assets(){
+  wp_register_style('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css');
+  wp_register_script('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js', ['popper', 'jquery'], false, true);
+  wp_deregister_script('jquery');
+  wp_register_script('popper', 'https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js', [], false, true);
+  wp_register_script('jquery', 'https://code.jquery.com/jquery-3.5.1.slim.min.js', [], false, true);
+  wp_enqueue_style('bootstrap');
+  wp_enqueue_script('bootstrap');
+}
+
+function montheme_document_title_parts($title) {
+  unset($title['tagline']);
+  return $title;
+}
+
+
+add_action('after_setup_theme', 'montheme_supports');
+add_action('wp_enqueue_scripts', 'montheme_register_assets');
+add_filter('document_title_parts', 'montheme_document_title_parts');
+
+?>
