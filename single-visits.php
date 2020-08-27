6dd39cbccd8633c75995751604060cba35efb080
lisaelyacoubi <lisa.elyacoubi@gmail.com>
2020-08-27 13:50:40 +0200

    display visits to zombieville

--- single-visits.php
+++ single-visits.php
@@ -1,11 +1,8 @@
 <?php get_header() ?>
-<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
-
-
+<?php
+if (have_posts()) : while (have_posts()) : the_post(); ?>
 
 <div class=" mb-5 pb-5 bg-zombiecity container border rounded">
-      <!-- EN TETE ARTICLE -->
       <section class="container">
         <h2 class="text-uppercase text-center pt-5 pb-2"><?php the_title() ?></h2>
         <a href="#" class="mb-5 d-flex justify-content-center"><?php the_category() ?></a>
@@ -13,9 +10,8 @@
           <?php the_post_thumbnail('medium_large'); ?>
         </div>
       </section>
-      <!-- EN TETE ARTICLE -->
       <section class="container pt-5">
-        <p class="text-center">  <?php the_content() ?></p>
+        <p class="text-center"><?php the_content() ?></p>
       </section>
     </div>
 
@@ -26,6 +22,5 @@
     </section>
 
 <?php endwhile; endif ?>
-
 
 <?php get_footer() ?>
